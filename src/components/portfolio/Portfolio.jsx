d7a9476dd6f0cd43649420811467784057a56535
Deepak <dsahni633@gmail.com>
2023-08-31 23:11:49 +0530

    added projects bg

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './portfolio.css'
 import PortfolioItems from './PortfolioItems'
-import IMG1 from '../../images/bg.png'
-import IMG2 from '../../images/bg.png'
-import IMG3 from '../../images/bg.png'
-import IMG4 from '../../images/bg.png'
-import IMG5 from '../../images/bg.png'
+import IMG1 from '../../images/bg1.png'
+import IMG2 from '../../images/bg2.png'
+import IMG3 from '../../images/bg3.png'
+import IMG4 from '../../images/bg4.png'
+import IMG5 from '../../images/bg5.png'
 
 function Portfolio() {
   return (
@@ -14,11 +14,10 @@ function Portfolio() {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-         <PortfolioItems image={IMG1} title="The title" link="https://github.com"/>
-         <PortfolioItems image={IMG2} title="The title" link="https://github.com"/>
-         <PortfolioItems image={IMG3} title="The title" link="https://github.com"/>
-         <PortfolioItems image={IMG4} title="The title" link="https://github.com"/>
-         <PortfolioItems image={IMG5} title="The title" link="https://github.com"/>
+         <PortfolioItems image={IMG1} title="Portfolio-Website" link="deepaksahni.netlify.app"/>
+         <PortfolioItems image={IMG2} title="iNotebook-To-Do App" link="inoteboook.netlify.app"/>
+         <PortfolioItems image={IMG3} title="Responsive-Website" link="responsivewpage.netlify.app/"/>
+         <PortfolioItems image={IMG4} title="NetFlix clone" link="https://clonenetflixsite.netlify.app/"/> 
       </div>
     </section>
   )
